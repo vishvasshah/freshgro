0210b63628d616cf45faf1ad83530c42ba239bb0
vishvas shah <vishvasshah2016@gmail.com>
2022-04-08 01:57:04 +0530

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {} from "@mui/material"
+import { BarChart, SearchRounded, ShoppingCartRounded } from "@mui/icons-material";
+function Header() {
+    return <header>
+        <img className="logo" src="https://firebasestorage.googleapis.com/v0/b/freshgro-6d248.appspot.com/o/1601881328936.jpg?alt=media&token=0eac3044-5498-4d64-8a16-5b81f70538a2" alt=""/>
+        <div className="inputBox">
+            <SearchRounded className="searchIcon"/>
+            <input type="text" placeholder="search"/>
+        </div>
+
+        <div className="shoppingCart">
+            <ShoppingCartRounded className="cart"/>
+            <div className="cart_content">
+                <p>2</p>
+            </div>
+            
+        </div>
+
+       <div className="profileContainer"><h4>vishwas shah</h4></div>
+        <div className="toggleMenu">
+            <BarChart className="toggleIcon"/>
+        </div>
+    </header>;
+}
+export default Header;
